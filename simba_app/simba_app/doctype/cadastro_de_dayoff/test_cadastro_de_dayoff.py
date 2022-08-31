998cb238220cd9c010c0fe362e9b8f6b06a7efd9
ramirezmz <roberto.paolo@dremio.com>
2022-08-31 10:14:18 -0300

    feat: add cadastro de day off form

---
+++ simba_app/simba_app/doctype/cadastro_de_dayoff/test_cadastro_de_dayoff.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Vinicius Roque and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCadastrodeDayoff(FrappeTestCase):
+	pass
